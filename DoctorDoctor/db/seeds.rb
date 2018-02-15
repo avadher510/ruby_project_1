150646f08b5058be1b6aabb6669c04a425742a88
Anish Vadher <avadher510@gmail.com>
2018-02-15 18:05:56 +0000

    doctors seeds added

--- DoctorDoctor/db/seeds.rb
+++ DoctorDoctor/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Doctor.delete_all
+
+d1 = Doctor.create({name: "Mary-Jo Foster", specialism: "General Practice", location: "London Medical Practice", gender: "female", summary: "40 years experience within the community"})
+
+d2 = Doctor.create({name: "Jodie Marmaduke", specialism: "Neurology", location: "Birmingham Medical Practice", gender: "female", summary: "10 years into my specialism. Active researcher."})
+
+d3 = Doctor.create({name: "Rajesh Patel", specialism: "Hepatology", location: "Leicester Medical Practice", gender: "male", summary: "33 years experience. Consultant within the field"})
+
+d4 = Doctor.create({name: "Mike Brown", specialism: "Cardiology", location: "Nottingham Medical Practice", gender: "male", summary: "4 years experience in Cardiothoracic Surgery. Active researcher."})
+
+d5 = Doctor.create({name: "Melissa Jefferies", specialism: "Paediatrics", location: "Bradford Medical Practice", gender: "female", summary: "6 years experience. Passion for helping suffering children back to health."})
+
+d6 = Doctor.create({name: "Pierre Vincent", specialism: "Nephrology", location: "Cambridge Medical Practice", gender: "male", summary: "Study of Nephrology fascinates me - I actively research to help advnace the field. 18 years experience."})
